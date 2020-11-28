fcf01da1a7304a623a4360d7b5c4c8a24f076e60
michael <michael@localhost.localdomain>
2020-11-29 00:05:52 +0100

    Do not compute condition number

--- CDMATH/tests/validation/scripts/WaveSystemStaggered/WaveSystemStaggered.py
+++ CDMATH/tests/validation/scripts/WaveSystemStaggered/WaveSystemStaggered.py
@@ -249,7 +249,6 @@ def WaveSystemStaggered(ntmax, tmax, cfl, my_mesh, output_freq, meshName, resolu
         LS=cdmath.LinearSolver(divMat,Un,iterGMRESMax, precision, "GMRES","ILU")
     else:
         LS=cdmath.LinearSolver(divMat,Vn,iterGMRESMax, precision, "GMRES","ILU")
-    LS.setComputeConditionNumber()
 
     test_desc["Linear_solver_algorithm"]=LS.getNameOfMethod()
     test_desc["Linear_solver_preconditioner"]=LS.getNameOfPc()
@@ -285,7 +284,6 @@ def WaveSystemStaggered(ntmax, tmax, cfl, my_mesh, output_freq, meshName, resolu
         
         test_desc["Linear_system_max_actual_iterations_number"]=max(LS.getNumberOfIter(),test_desc["Linear_system_max_actual_iterations_number"])
         test_desc["Linear_system_max_actual_error"]=max(LS.getResidu(),test_desc["Linear_system_max_actual_error"])
-        test_desc["Linear_system_max_actual_condition number"]=max(LS.getConditionNumber(),test_desc["Linear_system_max_actual_condition number"])
 
         max_dp=0 ;        max_dq=0
         for k in range(nbCells):
@@ -329,10 +327,10 @@ def WaveSystemStaggered(ntmax, tmax, cfl, my_mesh, output_freq, meshName, resolu
             velocity_field.writeVTK("WaveSystem"+str(dim)+"DStaggered"+meshName+"_velocity",False);
 
             print("Ecart au stationnaire exact : error_p= ",delta_press/p0," error_||u||= ",delta_v.maxVector()[0])
-            print
-    print"-- Iter: " + str(it) + ", Time: " + str(time) + ", dt: " + str(dt)
+            print( )
+    print("-- Iter: " + str(it) + ", Time: " + str(time) + ", dt: " + str(dt) )
     print("Variation temporelle relative : pressure ", max_dp/p0 ,", velocity ", max_dq/rho0 )
-    print
+    print( )
 
     if(it>=ntmax):
         print("Nombre de pas de temps maximum ntmax= ", ntmax, " atteint")
