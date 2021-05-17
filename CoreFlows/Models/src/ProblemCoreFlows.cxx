90e5b0270cea80ecdc857d5f57e7aa8c5a12467b
michael <michael@localhost.localdomain>
2021-05-17 13:43:00 +0200

    Corrected prototype of function setLinearSolver

--- CoreFlows/Models/src/ProblemCoreFlows.cxx
+++ CoreFlows/Models/src/ProblemCoreFlows.cxx
@@ -368,9 +368,9 @@ Mesh ProblemCoreFlows::getMesh()
 	return _mesh;
 }
 
-void ProblemCoreFlows::setLinearSolver(linearSolver kspType, preconditioner pcType)
+void ProblemCoreFlows::setLinearSolver(linearSolver kspType, preconditioner pcType, double maxIts)
 {
-	//_maxPetscIts=maxIterationsPetsc;
+	_maxPetscIts=maxIts;
 	// set linear solver algorithm
 	if (kspType==GMRES)
 		_ksptype = (char*)&KSPGMRES;
@@ -586,6 +586,7 @@ bool ProblemCoreFlows::solveTimeStep(){
 
 	return converged;
 }
+
 ProblemCoreFlows::~ProblemCoreFlows()
 {
 	/*
