cb928906128e52bff2d895f967b8977789155745
Martin BERNHARD <martin.bernhard@opencascade.com>
2026-01-06 12:15:17 +0000

    Updated year in copyright comment

--- solverlabGUI/solverlabpy/i18n/test_190_i18n.py
+++ solverlabGUI/solverlabpy/i18n/test_190_i18n.py
@@ -2,7 +2,7 @@
 #-*- coding:utf-8 -*-
 
 # %% LICENSE_SALOME_CEA_BEGIN
-# Copyright (C) 2008-2025  CEA
+# Copyright (C) 2008-2026  CEA
 # 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
